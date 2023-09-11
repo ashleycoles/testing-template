0e59eda7e932613a67a64d75e6881041f8273410
Ashley Coles <ashley@acoles.co.uk>
2023-09-11 08:46:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
 
-    <title>Template</title>
+function testing(int $b, int $c,
+                 int $g,
+                 int $h):string{
+    return 'test';
+}
 
-    <meta name="description" content="Template HTML file">
-    <meta name="author" content="iO Academy">
+class FOo{
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-    <link rel="stylesheet" href="css/styles.css">
-
-    <link rel="icon" href="images/favicon.png" sizes="192x192">
-    <link rel="shortcut icon" href="images/favicon.png">
-    <link rel="apple-touch-icon" href="images/favicon.png">
-
-    <script defer src="js/index.js"></script>
-</head>
-
-<body>
-
-<h1>Website Template</h1>
-
-</body>
-</html>
+}
